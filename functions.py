90e5e1dfbdc4a442b55123bbcf8bd151931cdf62
Aditya Tiwari <thegeekyboy.in@gmail.com>
2022-09-17 22:36:31 +0530

    Indentation improved

--- functions.py
+++ functions.py
@@ -3,7 +3,7 @@ def is_leap(year):
     if year % 4 ==0:
         if year % 100 ==0:
             if year % 400==0:
-               return True
+                return True
             else:
                 return False
         else:
