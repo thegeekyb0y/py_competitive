0ee39e15e90cc47c127b07172c8cfa31ec0949f1
Tarun Balaji K S <65398433+guitaruser@users.noreply.github.com>
2022-10-02 19:00:19 +0530

    Create word_order.py

---
+++ word_order.py
@@ -0,0 +1,10 @@
+from collections import OrderedDict
+words = OrderedDict()
+
+for _ in range(int(input())):
+    word = input()
+    words.setdefault(word, 0)
+    words[word] += 1
+   
+print(len(words))
+print(*words.values())
